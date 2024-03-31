980e771ca24f57186f4e27850f4313e5059f090e
Vibhor Dubey <vibhordubey333@gmail.com>
2024-03-31 13:51:09 +0530

    Python-FastAPI-Mongo|Refactored API and readme

--- Python-FastAPI-Mongo/Readme.md
+++ Python-FastAPI-Mongo/Readme.md
@@ -6,5 +6,15 @@
 pip install pymongo
 pip 
 ```
-3. uvicorn main:app --reload
-4. Go to browser hit `http://localhost:8000/`
+3. Execute `python3 script.py`
+3. `uvicorn main:app --reload`
+4. Go to browser hit: <br/>
+   [] `http://localhost:8000/` <br/>
+   [] `http://127.0.0.1:8000/courses?sort=desc`
+5. To view swagger document.
+   `http://127.0.0.1:8000/docs`
+6. To view better docs
+   `http://127.0.0.1:8000/redoc`
+7. To get specific course
+   `http://127.0.0.1:8000/courses/66078d3122ae5cb883b0c46c`
+   To get above id, need to connect with mongo client.
--- Python-FastAPI-Mongo/main.py
+++ Python-FastAPI-Mongo/main.py
@@ -15,7 +15,8 @@ def index():
 
 
 @app.get('/courses')
-def getCourses(sort_by: str = 'date', domain: str = None):
+def get_courses(sort_by: str = 'date', domain: str = None):
+    # set the rating.total and rating.count to all the courses based on the sum of the chapters rating
     for course in db.courses.find():
         total = 0
         count = 0
@@ -25,13 +26,14 @@ def getCourses(sort_by: str = 'date', domain: str = None):
                 count += chapter['rating']['count']
         db.courses.update_one({'_id': course['_id']}, {'$set': {'rating': {'total': total, 'count': count}}})
 
+
     # sort_by == 'date' [DESCENDING]
     if sort_by == 'date':
         sort_field = 'date'
         sort_order = -1
 
-    # sort_by == 'date' [DESCENDING]
-    if sort_by == 'rating':
+    # sort_by == 'rating' [DESCENDING]
+    elif sort_by == 'rating':
         sort_field = 'rating.total'
         sort_order = -1
 
@@ -44,6 +46,32 @@ def getCourses(sort_by: str = 'date', domain: str = None):
     if domain:
         query['domain'] = domain
 
-    courses = db.courses.find(query, {'name': 1, 'date': 1, 'description': 1, 'domain': 1, 'rating': 1, 'id': 0}).sort(
-        sort_field, sort_order)
+
+    courses = db.courses.find(query, {'name': 1, 'date': 1, 'description': 1, 'domain':1,'rating':1,'_id': 0}).sort(sort_field, sort_order)
     return list(courses)
+
+# @app.get('/courses/{course_id}')
+# def get_course(course_id: str):
+#     # Retrieve course data from the database based on the provided course_id
+#     course = db.courses.find({'id':ObjectId(course_id)},{'_id':0,'chapters':0})
+#     # If the course is not found in the db, raise am HTTPException with status code 404
+#     if not course:
+#         raise HTTPException(status_code = 404,detail='COURSE_NOT_FOUND')
+#     # Try to extract the `total` rating from the 'rating' field of the course data
+#     try:
+#         course['rating']= course['rating']['total']
+#     # If total key doesn't exists in the rating field, set 'rating' to 'Not Rated'
+#     except KeyError:
+#         course['rating'] = 'Not rated yet'
+#     return course
+
+@app.get('/courses/{course_id}')
+def get_course(course_id: str):
+    course = db.courses.find_one({'_id': ObjectId(course_id)}, {'_id': 0, 'chapters': 0})
+    if not course:
+        raise HTTPException(status_code=404, detail='Course not found')
+    try:
+        course['rating'] = course['rating']['total']
+    except KeyError:
+        course['rating'] = 'Not rated yet'
+    return course
